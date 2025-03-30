59eafa50d60b1789c51c24de16bba316b26372d0
MrTerminal303 <trung.nt235444@sis.hust.edu.vn>
2025-03-30 23:28:44 +0700

    Fix the height of all components. Add the foundation to the add conversation button

--- client/src/app/(main)/layout.js
+++ client/src/app/(main)/layout.js
@@ -4,13 +4,13 @@ import { SidebarProvider } from "@/components/ui/sidebar";
 
 export default function Layout({ children }) {
     return (
-        <div className="flex flex-col h-screen w-screen gap-2 bg-gray-100 dark:bg-gray-900">
-            <AppHeader className="flex-none"/>
-            <div className="flex flex-1 gap-2 overflow-hidden dark:bg-gray-900">
-                <SidebarProvider className="flex-none w-64">
+        <div className="flex flex-col h-[100dvh] w-full overflow-hidden bg-gray-100 dark:bg-gray-900">
+            <AppHeader className="flex-none" />
+            <div className="flex flex-1 overflow-hidden dark:bg-gray-900">
+                <SidebarProvider className="flex-none w-64 overflow-hidden">
                     <AppSidebar />
                 </SidebarProvider>
-                <div className="flex-1 bg-gray-100 dark:bg-gray-900">
+                <div className="flex-1 overflow-hidden p-2">
                     {children}
                 </div>
             </div>

2387bcc3af3d904763d090287ddfc5f760530df2
BienKieu <bien14112005@gmail.com>
2025-04-09 12:03:01 +0700

    complete User

--- server/src/api/routes/index.js
+++ server/src/api/routes/index.js
@@ -1,14 +1,14 @@
 import { Router } from "express";
 
 import authRoute from "./auth-route.js";
-// import userRoute from "./user-route.js";
+import userRoute from "./user-route.js";
 // import conversationRoute from "./conversation-route.js";
 
 const router = Router();
 
 function apiRouter() {
   authRoute(router);
-  // userRoute(router);
+  userRoute(router);
 
   // conversationRoute(router);
 

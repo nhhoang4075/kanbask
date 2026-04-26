bb4274cf31086f1b0bc720d87b2d2fa8362d72a2
baocode2 <tranducbao2805@gmail.com>
2026-04-26 17:29:05 +0700

    feat: Add functional chat with send and receive messages to teams and users. Save messages, users, and conversations in JSON files and implement API routes to handle login, message sending, and conversation retrieval.

--- client/src/app/api/users/route.js
+++ client/src/app/api/users/route.js
@@ -5,19 +5,12 @@ import path from 'path';
 
 const usersFilePath = path.join(process.cwd(),'src', 'data', 'users.json');
 
-export async function POST(req) {
+export async function GET() {
     try {
-        const loginUser = await req.json();
         const data = await fsPromises.readFile(usersFilePath, 'utf-8');
         const users = JSON.parse(data);
-        const user = users.find(user => user.username === loginUser.username && user.password === loginUser.password);
-        if (user) {
-            return Response.json(user, { status: 200 });
-        } else {
-            return Response.json({ error: 'Invalid credentials' }, { status: 401 });
-        }
+        return Response.json(users, { status: 200 });
     } catch (error) {
-        console.error('Error validating user', error);
-        return Response.json({ error: 'Failed to validate' }, { status: 500 });
+        return Response.json({ error: 'Failed to fetch users' }, { status: 500 });
     }
 }

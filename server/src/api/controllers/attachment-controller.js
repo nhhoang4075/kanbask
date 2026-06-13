dc5062fd7bda7d1f8285b6dc136ecadd4461b4a0
Nguyễn Huy Hoàng <nhh4075@gmail.com>
2026-06-13 16:46:25 +0700

    refactor(multimedia): change single file uploading into multiple files uploading

--- server/src/api/controllers/attachment-controller.js
+++ server/src/api/controllers/attachment-controller.js
@@ -1,19 +1,19 @@
 import { StatusCodes } from "http-status-codes";
+
 import attachmentService from "../services/attachment-service.js";
-import ApiError from "../../utils/api-error.js";
 
 const downloadAttachment = async (req, res, next) => {
   try {
-    const { attachmentId } = req.params;
-    const userId = req.user.id;
+    const { attachment_id } = req.params;
 
-    const downloadUrl = await attachmentService.generateDownloadUrl(attachmentId, userId);
+    const download_url = await attachmentService.generateDownloadUrlOfAttachment(
+      attachment_id,
+      req.user.id
+    );
 
     res.status(StatusCodes.OK).json({
       success: true,
-      data: {
-        downloadUrl: downloadUrl,
-      },
+      data: { download_url }
     });
   } catch (error) {
     next(error);
@@ -22,39 +22,17 @@ const downloadAttachment = async (req, res, next) => {
 
 const deleteSystemAttachment = async (req, res, next) => {
   try {
-    const { attachmentId } = req.params;
+    const { attachment_id } = req.params;
     const userId = req.user.id;
 
-    const deletedAttachmentId = await attachmentService.deleteAttachmentFromSystem(attachmentId, userId);
+    const deletedAttachmentId = await attachmentService.deleteAttachmentFromSystem(
+      attachment_id,
+      userId
+    );
 
     res.status(StatusCodes.OK).json({
       success: true,
       message: `Attachment ${deletedAttachmentId} deleted successfully from system.`
-    });
-  } catch (error) {
-    next(error);
-  }
-};
-
-const UploadToTask = async (req, res, next) => {
-  try {
-    const { taskId } = req.params;
-    const userId = req.user.id;
-
-    const { buffer, originalname, mimetype, size } = req.file;
-
-    const attachmentRecord = await attachmentService.uploadFileAndRecord(
-      buffer,
-      originalname,
-      mimetype,
-      size,
-      userId
-    );
-
-    const link = await attachmentService.linkAttachmentToTask(taskId, attachmentRecord.id, userId);
-    res.status(StatusCodes.CREATED).json({
-      success: true,
-      data: link
     });
   } catch (error) {
     next(error);
@@ -90,7 +68,13 @@ const UploadToMessage = async (req, res, next) => {
       size,
       userId
     );
-    const link = await attachmentService.linkAttachmentToMessage(messageId, attachmentRecord.id, userId);
+
+    const link = await attachmentService.linkAttachmentToMessage(
+      messageId,
+      attachmentRecord.id,
+      userId
+    );
+
     res.status(StatusCodes.CREATED).json({
       success: true,
       data: link
@@ -115,11 +99,24 @@ const getMessageAttachments = async (req, res, next) => {
   }
 };
 
+const uploadAttachmentsToTask = async (req, res, next) => {
+  try {
+    await attachmentService.uploadAttachmentsToTask(req.params.taskId, req.files, req.user.id);
+
+    res.status(StatusCodes.OK).json({
+      success: true,
+      message: "OK"
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
 export default {
   downloadAttachment,
   deleteSystemAttachment,
-  UploadToTask,
   getTaskAttachments,
   UploadToMessage,
   getMessageAttachments,
+  uploadAttachmentsToTask
 };
--- server/src/api/validations/attachment-validation.js
+++ server/src/api/validations/attachment-validation.js
@@ -2,71 +2,45 @@ import { z } from "zod";
 import { validate } from "../../config/validator.js";
 
 const validateAttachmentIdParam = validate(
-  z.object({
-    params: z.object({
-      attachmentId: z.string().uuid("Invalid Attachment ID format (UUID expected).")
-    }).strict(),
-    body: z.object({}).optional(),
-    query: z.object({}).optional(),
-  }).strict()
-);
-
-const validateParentIdParam = (paramName = "id") => validate(
-  z.object({
-    params: z.object({
-      [paramName]: z.string()
+  z
+    .object({
+      params: z
+        .object({
+          attachment_id: z.string().uuid()
+        })
+        .strict(),
+      body: z.object({}).optional(),
+      query: z.object({}).optional()
     })
     .strict()
-    .refine(
-      (actualParams) => {
-        const idValue = actualParams[paramName];
-        return idValue && /^\d+$/.test(idValue) && parseInt(idValue, 10) > 0;
-      },
-      {
-        message: `Parameter '${paramName}' must be a positive integer.`,
-        path: [paramName]
-      }
-    ),
-    body: z.object({}).optional(),
-    query: z.object({}).optional(),
-  }).strict()
 );
 
-const validateLinkBody = validate(
-  z.object({
-    body: z.object({
-      attachmentId: z.string().uuid("Field 'attachmentId' in body must be a valid UUID.")
-    }).strict(),
-    params: z.any().optional(),
-    query: z.object({}).optional(),
-  }).strict()
-);
-
-const validateUnlinkParams = (parentParamName = "id") => validate(
-  z.object({
-    params: z.object({
-      [parentParamName]: z.string(),
-      attachmentId: z.string().uuid("Parameter 'attachmentId' must be a valid UUID.")
-    })
-    .strict()
-    .refine(
-      (actualParams) => {
-        const parentIdValue = actualParams[parentParamName];
-        return parentIdValue && /^\d+$/.test(parentIdValue) && parseInt(parentIdValue, 10) > 0;
-      },
-      {
-        message: `Parameter '${parentParamName}' must be a positive integer.`,
-        path: [parentParamName]
-      }
-    ),
-    body: z.object({}).optional(),
-    query: z.object({}).optional(),
-  }).strict()
-);
+const validateParentIdParam = (paramName = "id") =>
+  validate(
+    z
+      .object({
+        params: z
+          .object({
+            [paramName]: z.string()
+          })
+          .strict()
+          .refine(
+            (actualParams) => {
+              const idValue = actualParams[paramName];
+              return idValue && /^\d+$/.test(idValue) && parseInt(idValue, 10) > 0;
+            },
+            {
+              message: `Parameter '${paramName}' must be a positive integer.`,
+              path: [paramName]
+            }
+          ),
+        body: z.object({}).optional(),
+        query: z.object({}).optional()
+      })
+      .strict()
+  );
 
 export default {
   validateAttachmentIdParam,
-  validateParentIdParam,
-  validateLinkBody,
-  validateUnlinkParams
+  validateParentIdParam
 };
